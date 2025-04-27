5c4d6b82769abbcae3fa0e342894f87b7e745539
rigofekete <fabricecosteira@gmail.com>
2025-04-27 22:56:07 +0200

    added main.py and textnode.py files

---
+++ src/textnode.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL  = "text"
+    BOLD    = "bold"
+    ITALIC  = "italic"
+    CODE    = "code"
+    LINK    = "link"
+    IMAGE   = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text == other.text and
+            self.text_type == other.tex_type and
+            self.url == other.url
+                )
+
+    def __repr__(self):
+        # using the text_type.value is necessary to print out the value of the Enum, other wise python prints out the actual Enum class name and enumarator name (TextType.LINK)
+        return f"TextNode({self.text},{self.text_type.value}, {self.url})"
+
+
+
+
+
+
