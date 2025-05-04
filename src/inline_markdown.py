a87137b3e0d0130765a445994a506ba6d6bd94fa
rigofekete <fabricecosteira@gmail.com>
2025-05-04 23:00:26 +0200

    markdown_to_html_node.py update + helper functions added

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -4,7 +4,6 @@ from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-
     for node in old_nodes:
         if node.text_type is not TextType.TEXT:
             new_nodes.append(node)
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,7 +1,7 @@
 from enum import Enum 
 from htmlnode import HTMLNode, ParentNode
 from inline_markdown import text_to_text_nodes
-from textnode import text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node
 import re
 
 class BlockType(Enum):
@@ -89,7 +89,6 @@ def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     for block in blocks:
         html_node = type_to_html_node(block)
-        # if html_node: 
         children.append(html_node)
     return ParentNode("div", children)
 
@@ -99,13 +98,25 @@ def type_to_html_node(block):
     match(block_type):
         case BlockType.HEADING:
             return heading_to_html_node(block)
+        case BlockType.PARAGRAPH:
+            return paragraph_to_html_node(block)
+        case BlockType.CODE:
+            return code_to_html_node(block)
+        case BlockType.QUOTE:
+            return quote_to_html_node(block)
+        case BlockType.ULIST:
+            return ul_to_html_node(block)
+        case BlockType.OLIST:
+            return ol_to_html_node(block)
+        case _:
+            raise ValueError(f"invalid block type: {block_type}")
             
 def text_to_children(text):
     children = []
     nodes = text_to_text_nodes(text)
     for node in nodes:
-        print(f"NODE: {node}")
-        children.append(text_node_to_html_node(node))
+        html_node = text_node_to_html_node(node)
+        children.append(html_node)
     return children
 
 def heading_to_html_node(block):
@@ -120,6 +131,52 @@ def heading_to_html_node(block):
     text = block[level + 1:]
     children = text_to_children(text)
     return ParentNode(f"h{level}", children) 
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    text = " ".join(lines)
+    children = text_to_children(text)
+    return ParentNode("p", children)
+
+def code_to_html_node(block):
+    text = block.strip("```")
+    text = text[1:]
+    code_node = TextNode(text, TextType.CODE, None)
+    html_node = text_node_to_html_node(code_node)
+    return ParentNode("pre", [html_node])
+    
+def quote_to_html_node(block):
+    children = []
+    text = ""
+    quotes = block.split("\n")
+    for quote in quotes:
+        if quote.startswith(">"):
+            text += quote[1:]
+        else:
+            raise ValueError("invalid quote format, missing starting '>'")
+    children = text_to_children(text)
+    return ParentNode("blockquote", children)
+
+def ul_to_html_node(block):
+    children = []
+    text = ""
+    lists = block.split("\n")
+    for list in lists:
+        text += f"<ls>{list[2:]}<ls>"
+    children = text_to_children(text)
+    return ParentNode("ul", children)
+
+def ol_to_html_node(block):
+    children = []
+    text = ""
+    lists = block.split("\n")
+    for list in lists:
+        text += f"<ls>{list[3:]}<ls>"
+    children = text_to_children(text)
+    return ParentNode("ol", children)
+    
+
+
 
 
 #
@@ -149,16 +206,52 @@ def heading_to_html_node(block):
 # """
 #
 
+# md = """
+#
+# ```
+# this is a **paragraph** and I do not know what to do
+# I also dont know what to write
+# ```
+# """
+
 md = """
 
+This is **bolded** paragraph
+text in a p
+tag here
 
-### this is a header
-
+This is another paragraph with _italic_ text and `code` here
 
 """
 
+
+
+# md = """
+#
+# > I am the god of hellfire and I bring you... fire.
+# > quote unquote
+#
+# """
+
+# md = """
+#
+# - eat lunch
+# - drink coffee 
+# - clean the bathroom
+#
+# """
+
+# md = """
+#
+# 1. no coffee
+# 2. no wine
+# 3. maybe something sweet
+# 4. hit the sack
+#
+# """
+
 node = markdown_to_html_node(md)
-print(node)
+# print(f"NODE: {node}")
 html = node.to_html()
 
 print(html)
