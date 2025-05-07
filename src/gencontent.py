a663773176f5f2404cea86258381ebc4a5e00182
rigofekete <fabricecosteira@gmail.com>
2025-05-07 23:33:29 +0200

    gencontent.py update and github pages set up

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,74 +2,119 @@ import os
 import shutil
 from markdown_blocks import markdown_to_html_node
 
-# since we are executing the program from the root with the main.sh script we need to set the paths according to the main.sh location (root)
-from_path = "./content/index.md"
-template_path = "./template.html"
-dest_path = "./public/index.html"
 
-dir_path_content = "./content"
-dest_dir_path = "./public"
+# def generate_page(from_path, template_path, dest_path, basepath):
+#     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+#     markdown = ""
+#     template = ""
+#     if os.path.isfile(from_path): 
+#         file_obj = open(from_path)
+#         markdown = file_obj.read()
+#         file_obj.close()
+#     else: 
+#         raise ValueError(f"path is not a file: {from_path}")
+#     if os.path.isfile(template_path):
+#         template_obj = open(template_path)
+#         template = template_obj.read()
+#     else: 
+#         raise ValueError(f"path is not a file: {template_path}")
+#
+#     node = markdown_to_html_node(markdown)
+#
+#     html_string = node.to_html()
+#
+#     title = extract_title(markdown)
+#
+#     new_html_page = template.replace("{{ Title }}", title, 1).replace("{{ Content }}", html_string, 1)
+#
+#     with open(dest_path, "w") as file:
+#         file.write(new_html_page)
+    
+def generate_page(from_path, template_path, to_path, basepath):
+    print(f"Generating page from {from_path} to {to_path} using {template_path}")
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    markdown = ""
-    template = ""
-    if os.path.isfile(from_path): 
-        file_obj = open(from_path)
-        markdown = file_obj.read()
-        file_obj.close()
-    else: 
-        raise ValueError(f"path is not a file: {from_path}")
-    if os.path.isfile(template_path):
-        template_obj = open(template_path)
-        template = template_obj.read()
-    else: 
-        raise ValueError(f"path is not a file: {template_path}")
-
-    node = markdown_to_html_node(markdown)
-
-    html_string = node.to_html()
+    to_path = to_path.replace(".md", ".html", 1)
+    print(f" * {from_path} -> {to_path}")
+    file_obj = open(from_path, "r")
+    markdown = file_obj.read()
+    file_obj.close()
 
     title = extract_title(markdown)
 
-    new_html_page = template.replace("{{ Title }}", title, 1).replace("{{ Content }}", html_string, 1)
+    node = markdown_to_html_node(markdown)
+    html_content = node.to_html()
 
-    with open(dest_path, "w") as file:
-        file.write(new_html_page)
-    
+    template_obj = open(template_path, "r")
+    template = template_obj.read()
+    template_obj.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    final_html = template.replace("{{ Title }}", title, 1)
+    final_html = final_html.replace("{{ Content }}", html_content, 1)
+    html_basepath = final_html.replace('href="/', f'href="{basepath}')
+    html_basepath = html_basepath.replace('src="/', f'src="{basepath}')
+
+    file = open(to_path, "w")
+    file.write(html_basepath)
+    file.close()
+
+    # markdown = ""
+    # template = ""
+    # if os.path.isfile(from_path): 
+    #     file_obj = open(from_path)
+    #     markdown = file_obj.read()
+    #     file_obj.close()
+    # else: 
+    #     raise ValueError(f"path is not a file: {from_path}")
+    # if os.path.isfile(template_path):
+    #     template_obj = open(template_path)
+    #     template = template_obj.read()
+    # else: 
+    #     raise ValueError(f"path is not a file: {template_path}")
+    #
+    # node = markdown_to_html_node(markdown)
+    #
+    # html_string = node.to_html()
+    #
+    # title = extract_title(markdown)
+    #
+    # new_html_page = template.replace("{{ Title }}", title, 1).replace("{{ Content }}", html_string, 1)
+    #
+    # with open(dest_path, "w") as file:
+    #     file.write(new_html_page)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
        
-    print(f"DIR PATH CONTENT: {dir_path_content}")
     for entry in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, entry) 
         to_path = os.path.join(dest_dir_path, entry)
         if os.path.isfile(from_path) and entry.endswith(".md"):
-            to_path = to_path.replace(".md", ".html", 1)
-            print(f" * {from_path} -> {to_path}")
-            file_obj = open(from_path, "r")
-            markdown = file_obj.read()
-            file_obj.close()
-            
-            title = extract_title(markdown)
-
-            node = markdown_to_html_node(markdown)
-            html_content = node.to_html()
-
-            template_obj = open(template_path, "r")
-            template = template_obj.read()
-            template_obj.close()
-
-            final_html = template.replace("{{ Title }}", title, 1)
-            final_html = final_html.replace("{{ Content }}", html_content, 1)
-
-            file = open(to_path, "w")
-            file.write(final_html)
-            file.close()
+            generate_page(from_path, template_path, to_path, basepath)
+            # to_path = to_path.replace(".md", ".html", 1)
+            # print(f" * {from_path} -> {to_path}")
+            # file_obj = open(from_path, "r")
+            # markdown = file_obj.read()
+            # file_obj.close()
+            #
+            # title = extract_title(markdown)
+            #
+            # node = markdown_to_html_node(markdown)
+            # html_content = node.to_html()
+            #
+            # template_obj = open(template_path, "r")
+            # template = template_obj.read()
+            # template_obj.close()
+            #
+            # final_html = template.replace("{{ Title }}", title, 1)
+            # final_html = final_html.replace("{{ Content }}", html_content, 1)
+            #
+            #
+            # file = open(to_path, "w")
+            # file.write(final_html)
+            # file.close()
         else:
-            generate_pages_recursive(from_path, template_path, to_path)
+            generate_pages_recursive(from_path, template_path, to_path, basepath)
             
 
 
@@ -89,11 +134,6 @@ def extract_title(md):
         if line.startswith("# "):
             return line[2:]
     raise ValueError("no title found")
-
-
-# generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
-
-
 
 
 
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import shutil
 from copystatic import cpy_static_to_public
@@ -6,12 +7,18 @@ from gencontent import generate_pages_recursive
 
 # since we are executing the program from the root with the main.sh script we need to set the paths according to the main.sh location (root)
 static_path = "./static"
-public_path = "./public"
+public_path = "./docs"
 
 from_path = "./content"
 template_path = "./template.html"
-dest_path = "./public"
+dest_path = "./docs"
 
+# static_path = "../static"
+# public_path = "../public"
+#
+# from_path = "../content"
+# template_path = "../template.html"
+# dest_path = "../public"
 
 def main():
     if os.path.exists(public_path):
@@ -21,8 +28,15 @@ def main():
     print("Copying static files to public directory...")
     cpy_static_to_public(public_path, static_path)
 
-    # generate_page(from_path, template_path, to_path)
-    generate_pages_recursive(from_path, template_path, dest_path)
+    basepath = ""
+    for arg in sys.argv:
+        if not arg:
+            basepath = "/"
+            break
+        basepath = arg
+        break
+
+    generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
